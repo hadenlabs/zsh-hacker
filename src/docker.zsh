530434691ca9be6b54780fbd9a28c1e181eb38e5
@slovacus <slovacus@gmail.com>
2020-04-26 20:56:45 -0500

    chore: implement command for sqlmap (#5)

---
+++ src/docker.zsh
@@ -0,0 +1,9 @@
+#!/usr/bin/env ksh
+# -*- coding: utf-8 -*-
+
+# Takes in $1 the python file to execute with sqlmap
+function sqlmap {
+    docker run --rm -it \
+           --volume ~/.sqlmap:/root/.sqlmap --volume "$(pwd)":/data --volume /tmp:/tmp \
+           booyaabes/kali-linux-full sqlmap "${@}"
+}
