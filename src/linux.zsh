40400e762fe2ca24fb1132f05db9960bd8afc519
@slovacus <slovacus@gmail.com>
2020-04-26 20:51:28 -0500

    chore: implement code for source (#5)

---
+++ src/linux.zsh
@@ -0,0 +1,7 @@
+#!/usr/bin/env ksh
+# -*- coding: utf-8 -*-
+
+# docker::install - install docker
+function docker::install {
+        message_warning "${FUNCTIONS_MESSAGE_NOT_FOUND}"
+}
---
+++ zsh-hacker.zsh
@@ -0,0 +1,39 @@
+#!/usr/bin/env ksh
+# -*- coding: utf-8 -*-
+
+#
+# Defines functions hacker for osx or linux.
+#
+# Authors:
+#   Luis Mayta <slovacus@gmail.com>
+#
+#
+HACKER_PLUGIN_DIR="$(dirname "${0}")"
+HACKER_SOURCE_PATH="${HACKER_PLUGIN_DIR}"/src
+
+export HACKER_MESSAGE_BREW="Please install brew or use antibody bundle luismayta/zsh-brew branch:develop"
+export HACKER_MESSAGE_NOT_FOUND="this not found installed"
+
+# shellcheck source=/dev/null
+source "${HACKER_SOURCE_PATH}"/base.zsh
+
+# shellcheck source=/dev/null
+source "${HACKER_SOURCE_PATH}"/utils.zsh
+
+# cross::os functions for osx and linux
+function cross::os {
+
+    case "${OSTYPE}" in
+        darwin*)
+            # shellcheck source=/dev/null
+            source "${HACKER_SOURCE_PATH}"/osx.zsh
+            ;;
+        linux*)
+            # shellcheck source=/dev/null
+            source "${HACKER_SOURCE_PATH}"/linux.zsh
+            ;;
+    esac
+
+}
+
+cross::os
