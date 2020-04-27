428c7cea0833b08d2c255d09127730084cf254b8
@slovacus <slovacus@gmail.com>
2020-04-26 20:51:28 -0500

    chore: implement code for source (#5)

---
+++ src/osx.zsh
@@ -0,0 +1,13 @@
+#!/usr/bin/env ksh
+# -*- coding: utf-8 -*-
+
+# docker::install - install docker
+function docker::install {
+    if ! type -p brew > /dev/null; then
+        message_warning "${FUNCTIONS_MESSAGE_BREW}"
+        return
+    fi
+    message_info "Install docker"
+    brew cask install docker
+    message_success "Installed docker"
+}
