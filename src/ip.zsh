225f7565ae72cba1f25153ea6313e0b95e315c9a
@slovacus <slovacus@gmail.com>
2020-04-28 03:40:30 -0500

    chore: implement load functions services (#9)

--- src/ip.zsh
+++ src/ip.zsh
@@ -1,14 +1,18 @@
 #!/usr/bin/env ksh
 # -*- coding: utf-8 -*-
 
-function ipaddressreport {
-    local ipaddress
+function hacker::ip::validate {
     if [ -z "${VIRUSTOTAL_API_KEY}" ]; then
         message_warning "VIRUSTOTAL_API_KEY is neccesary"
         return 0
     fi
+}
+
+function ipaddressreport {
+    local ipaddress
+    hacker::ip::validate
     ipaddress="${1}"
 
     http GET https://www.virustotal.com/vtapi/v2/ip-address/report \
          apikey=="${VIRUSTOTAL_API_KEY}" ip=="${ipaddress}"
-} 
+}
