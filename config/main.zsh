32b65c5b26d9afcbf27fc2ad6fe3df23f40981bf
@slovacus <slovacus@gmail.com>
2020-09-03 02:19:18 -0500

    chore: implement load functions for so (#16)

---
+++ config/main.zsh
@@ -0,0 +1,19 @@
+#!/usr/bin/env ksh
+# -*- coding: utf-8 -*-
+
+function hacker::config::main::factory {
+    # shellcheck source=/dev/null
+    source "${ZSH_HACKER_SRC_PATH}"/config/base.zsh
+    case "${OSTYPE}" in
+    darwin*)
+        # shellcheck source=/dev/null
+        source "${ZSH_HACKER_SRC_PATH}"/config/osx.zsh
+        ;;
+    linux*)
+        # shellcheck source=/dev/null
+        source "${ZSH_HACKER_SRC_PATH}"/config/linux.zsh
+      ;;
+    esac
+}
+
+hacker::config::main::factory
