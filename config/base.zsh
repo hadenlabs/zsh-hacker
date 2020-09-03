eb6090e65a93a389d6cb7772e472c978341ebd6e
Luis Mayta <luis@hadenlabs.com>
2020-09-03 02:19:18 -0500

    chore: implement load functions for so (#16)

---
+++ config/base.zsh
@@ -0,0 +1,5 @@
+#!/usr/bin/env ksh
+# -*- coding: utf-8 -*-
+
+export HACKER_MESSAGE_BREW="Please install brew or use antibody bundle luismayta/zsh-brew branch:develop"
+export HACKER_MESSAGE_NOT_FOUND="this not found installed"
