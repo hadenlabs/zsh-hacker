bd39d8fccb66ae5cdcbfe9003c77dd02c0b298f2
@slovacus <slovacus@gmail.com>
2020-09-03 02:18:36 -0500

    refactor: load functions for pkg and core (#16)

--- zsh-hacker.zsh
+++ zsh-hacker.zsh
@@ -8,50 +8,17 @@
 #   Luis Mayta <slovacus@gmail.com>
 #
 #
-HACKER_PLUGIN_DIR="$(dirname "${0}")"
-HACKER_SOURCE_PATH="${HACKER_PLUGIN_DIR}"/src
+ZSH_HACKER_SRC_PATH="$(dirname "${0}")"
 
-export HACKER_MESSAGE_BREW="Please install brew or use antibody bundle luismayta/zsh-brew branch:develop"
-export HACKER_MESSAGE_NOT_FOUND="this not found installed"
 
 # shellcheck source=/dev/null
-source "${HACKER_SOURCE_PATH}"/base.zsh
+source "${ZSH_HACKER_SRC_PATH}"/config/main.zsh
 
 # shellcheck source=/dev/null
-source "${HACKER_SOURCE_PATH}"/utils.zsh
-
-# cross::os functions for osx and linux
-function cross::os {
-
-    case "${OSTYPE}" in
-        darwin*)
-            # shellcheck source=/dev/null
-            source "${HACKER_SOURCE_PATH}"/osx.zsh
-            ;;
-        linux*)
-            # shellcheck source=/dev/null
-            source "${HACKER_SOURCE_PATH}"/linux.zsh
-            ;;
-    esac
-
-}
-
-cross::os
+source "${ZSH_HACKER_SRC_PATH}"/core/main.zsh
 
 # shellcheck source=/dev/null
-source "${HACKER_SOURCE_PATH}"/docker.zsh
+source "${ZSH_HACKER_SRC_PATH}"/internal/main.zsh
 
 # shellcheck source=/dev/null
-source "${HACKER_SOURCE_PATH}"/ip.zsh
-
-# shellcheck source=/dev/null
-source "${HACKER_SOURCE_PATH}"/domain.zsh
-
-# shellcheck source=/dev/null
-source "${HACKER_SOURCE_PATH}"/file.zsh
-
-# shellcheck source=/dev/null
-source "${HACKER_SOURCE_PATH}"/urls.zsh
-
-# shellcheck source=/dev/null
-source "${HACKER_SOURCE_PATH}"/merge.zsh
+source "${ZSH_HACKER_SRC_PATH}"/pkg/main.zsh
