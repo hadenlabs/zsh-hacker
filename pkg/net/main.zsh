07c8dac45dad61e4b69caecb1f861a496704468a
Luis Mayta <luis@hadenlabs.com>
2020-09-08 23:53:54 -0500

    chore: implement path for net (#17)

---
+++ pkg/net/main.zsh
@@ -0,0 +1,19 @@
+#!/usr/bin/env ksh
+# -*- coding: utf-8 -*-
+
+function hacker::pkg::net::main::factory {
+    # shellcheck source=/dev/null
+    source "${ZSH_HACKER_SRC_PATH}"/pkg/net/base.zsh
+    case "${OSTYPE}" in
+    darwin*)
+        # shellcheck source=/dev/null
+        source "${ZSH_HACKER_SRC_PATH}"/pkg/net/osx.zsh
+        ;;
+    linux*)
+        # shellcheck source=/dev/null
+        source "${ZSH_HACKER_SRC_PATH}"/pkg/net/linux.zsh
+      ;;
+    esac
+}
+
+hacker::pkg::net::main::factory
