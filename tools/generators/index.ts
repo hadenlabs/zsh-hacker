e5d0a6f00f5e796b0da3ebb30f423dbfb3308213
Luis Mayta <luis@hadenlabs.com>
2020-09-12 23:16:07 -0500

    chore: implement plop structure (#21)

--- tools/generators/index.ts
+++ tools/generators/index.ts
@@ -1,8 +1,9 @@
-import packageGenerator from './package/index'
-
+import { NodePlopAPI } from 'plop'
+import { packageGenerator } from './package'
 import { hyphenate } from './helpers'
 
-export = (plop: any) => {
+export default function main(plop: NodePlopAPI) {
+  plop.setWelcomeMessage('Welcome zsh hacker.')
   plop.setHelper('hyphenate', hyphenate)
   plop.setGenerator('package', packageGenerator)
 }
