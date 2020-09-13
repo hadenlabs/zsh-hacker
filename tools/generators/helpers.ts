b47e07970d3995455f1500ad1512d50475684bdc
@slovacus <slovacus@gmail.com>
2020-09-13 17:11:03 -0500

    fix: implement type return (#21)

--- tools/generators/helpers.ts
+++ tools/generators/helpers.ts
@@ -1,3 +1,3 @@
-export const hyphenate = (text: string) => {
-  return text.replace(/([a-zA-Z])(?=[A-Z])/g, '$1-').toLowerCase();
-};
+export const hyphenate = (text: string): string => {
+  return text.replace(/([a-zA-Z])(?=[A-Z])/g, '$1-').toLowerCase()
+}
