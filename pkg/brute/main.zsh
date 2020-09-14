23a65f761ef24a96dc34c09444a1f2e30452db74
@slovacus <slovacus@gmail.com>
2020-09-13 19:01:45 -0500

    chore: implement brute force (#20)

---
+++ pkg/brute/main.zsh
@@ -0,0 +1,19 @@
+#!/usr/bin/env ksh
+# -*- coding: utf-8 -*-
+
+function hacker::pkg::brute::factory {
+    # shellcheck source=/dev/null
+    source "${ZSH_HACKER_SRC_PATH}"/pkg/brute/base.zsh
+    case "${OSTYPE}" in
+    darwin*)
+        # shellcheck source=/dev/null
+        source "${ZSH_HACKER_SRC_PATH}"/pkg/brute/osx.zsh
+        ;;
+    linux*)
+        # shellcheck source=/dev/null
+        source "${ZSH_HACKER_SRC_PATH}"/pkg/brute/linux.zsh
+      ;;
+    esac
+}
+
+hacker::pkg::brute::factory
