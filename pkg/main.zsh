0de24b102de7404eba638cc5a2a34f1184e7a8e6
@slovacus <slovacus@gmail.com>
2020-09-13 19:21:14 -0500

    refactor: load packages tool for plop append code (#20)

--- pkg/main.zsh
+++ pkg/main.zsh
@@ -1,18 +1,14 @@
 #!/usr/bin/env ksh
 # -*- coding: utf-8 -*-
 
-function hacker::pkg::main::factory {
-    # shellcheck source=/dev/null
-    source "${ZSH_HACKER_SRC_PATH}"/pkg/tool/main.zsh
+# shellcheck source=/dev/null
+source "${ZSH_HACKER_SRC_PATH}"/pkg/tool/main.zsh
 
-    # shellcheck source=/dev/null
-    source "${ZSH_HACKER_SRC_PATH}"/pkg/research/main.zsh
+# shellcheck source=/dev/null
+source "${ZSH_HACKER_SRC_PATH}"/pkg/research/main.zsh
 
-    # shellcheck source=/dev/null
-    source "${ZSH_HACKER_SRC_PATH}"/pkg/net/main.zsh
+# shellcheck source=/dev/null
+source "${ZSH_HACKER_SRC_PATH}"/pkg/net/main.zsh
 
-    # shellcheck source=/dev/null
-    source "${ZSH_HACKER_SRC_PATH}"/pkg/brute/main.zsh
-}
-
-hacker::pkg::main::factory
+# shellcheck source=/dev/null
+source "${ZSH_HACKER_SRC_PATH}"/pkg/brute/main.zsh
