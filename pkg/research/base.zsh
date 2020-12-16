e0107b078789b7268bbaefa707dafffc569b9b9d
@slovacus <slovacus@gmail.com>
2020-12-16 11:28:55 -0500

    chore: implement function googler list (#28)

--- pkg/research/base.zsh
+++ pkg/research/base.zsh
@@ -6,3 +6,16 @@ function sherlock {
     username="${1}"
     docker run theyahya/sherlock "${username}" | fzf
 }
+
+function googler::list {
+    local site filetype name
+    site="${1}"
+    filetype="${2}"
+    name="${3}"
+
+    if [ -z "${name}" ] ; then
+        googler -w "${site}" -n 20 --json "filetype:${filetype}" | jq '.[].url' | sed -e 's/^"//' -e 's/"$//'
+        return
+    fi
+    googler -w "${site}"  -n 20 --json "filetype:${filetype}" "${name}" |jq '.[].url' | sed -e 's/^"//' -e 's/"$//'
+}
