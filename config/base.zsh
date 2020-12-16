cddaadb3477949f3adaed6bdea8e0e9dfecbfe01
Luis Mayta <luis@hadenlabs.com>
2020-12-16 11:11:04 -0500

    chore: implement packer name (#28)

--- config/base.zsh
+++ config/base.zsh
@@ -1,5 +1,6 @@
 #!/usr/bin/env ksh
 # -*- coding: utf-8 -*-
 
+export HACKER_PACKAGE_NAME=hacker
 export HACKER_MESSAGE_BREW="Please install brew or use antibody bundle luismayta/zsh-brew branch:develop"
 export HACKER_MESSAGE_NOT_FOUND="this not found installed"
